3a55c360fc392e0ac1445e8b6f7029d8e68cb577
xfcgby <74852606+xfcgby@users.noreply.github.com>
2025-09-08 16:42:11 +0800

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -51,12 +51,22 @@ const Player = () => {
       )
     }
   }
-
   useEffect(() => {
     initMusicPlayer()
     return () => {
       setPlayer(undefined)
     }
+    const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    lrcType: 1,
+    audio: {
+        name: 'name',
+        artist: 'artist',
+        url: 'demo.mp3',
+        cover: 'demo.jpg',
+        lrc: '[00:00.00]APlayer\n[00:04.01]is\n[00:08.02]amazing'
+    }
+});
   }, [])
 
   return (
