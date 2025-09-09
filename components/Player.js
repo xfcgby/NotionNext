6426535f04598d5c1629d2d2633586e86fcd9348
xfcgby <74852606+xfcgby@users.noreply.github.com>
2025-09-09 09:45:02 +0800

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -23,17 +23,7 @@ const Player = () => {
     'MUSIC_PLAYER_METING_CDN_URL',
     'https://cdnjs.cloudflare.com/ajax/libs/meting/2.0.1/Meting.min.js'
   )
-  const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    lrcType: 1,
-    audio: {
-        name: 'name',
-        artist: 'artist',
-        url: 'demo.mp3',
-        cover: 'demo.jpg',
-        lrc: '[00:00.00]APlayer\n[00:04.01]is\n[00:08.02]amazing'
-    }
-});
+ 
   const initMusicPlayer = async () => {
     if (!musicPlayerEnable) {
       return
